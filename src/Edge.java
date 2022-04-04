d2048c9edcb951c188573c8faae55982c6eccff0
gho8888 <smgho@ruc.dk>
2022-04-04 14:33:06 +0200

    Commented Edge and Town classes

--- src/Edge.java
+++ src/Edge.java
@@ -3,9 +3,9 @@ class Edge{
     Town to;
     Integer dist;
     public Edge(Town from,Town to, Integer dist){
-        this.from=from;
-        this.to=to;
-        this.dist=dist;
-        from.OutEdges.add(this);
+        this.from = from;
+        this.to = to;
+        this.dist = dist;
+        from.OutEdges.add(this); //adds an edge to the list of OutEdges ???
     }
 }
