c273a5ef61498a282e79fe7da2ff1a94e8f75e00
Victornrib <victornrib@gmail.com>
2022-04-04 16:06:44 +0200

    Running time added

--- src/AdjacencyGraph.java
+++ src/AdjacencyGraph.java
@@ -34,6 +34,7 @@ public class AdjacencyGraph {
 
     //Minimum spanning tree... Prims algorithm
     public void PrimsMST(){
+        long startTime = System.nanoTime();
         MinHeap<Town> Queue = new MinHeap<Town>();
 
         //Only runs algorithm if Towns<> is not empty
@@ -84,6 +85,9 @@ public class AdjacencyGraph {
         else {
             System.out.println("There are no Towns in this Adjacency Graph.");
         }
+        long endTime   = System.nanoTime();
+        long totalTime = endTime - startTime;
+        System.out.println("\nRunning time of PrimsMST: " + totalTime + " ns.");
     }
 }
 
